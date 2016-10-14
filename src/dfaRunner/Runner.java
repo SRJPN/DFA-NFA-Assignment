9f59ed3bae676b3bea020db7fb00e6d053082b2f
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-10-14 17:29:04 +0530

    Sooraj: moved out to a method runSingleString and handling exception

--- src/dfaRunner/Runner.java
+++ src/dfaRunner/Runner.java
@@ -4,6 +4,7 @@ import dfaAssignment.DFA;
 import dfaAssignment.DfaGenerator;
 import dfaAssignment.InvalidTupleException;
 import dfaAssignment.tuple.Alphabets;
+import dfaAssignment.tuple.NoSuchTrassitionException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -25,27 +26,33 @@ public class Runner {
             JSONObject jsonObject = (JSONObject) o;
             try {
                 System.out.println("\n->\t"+jsonObject.get("name").toString()+" is being analysed.....");
-                DFA dfa = dfaGenerator.fromJson((JSONObject) jsonObject.get("tuple"));
-                ArrayList<Alphabets> passCases = dfaGenerator.parseCases((JSONArray) jsonObject.get("pass-cases"));
-                for (Alphabets alphabets : passCases) {
-                    check(true, alphabets, dfa);
-                }
-
-                ArrayList<Alphabets> failCases = dfaGenerator.parseCases((JSONArray) jsonObject.get("fail-cases"));
-                for (Alphabets alphabets : failCases) {
-                    check(false, alphabets, dfa);
-                }
+                runSingleString(dfaGenerator, jsonObject);
                 System.out.println("\t\tchecked.... OK ✓");
             }catch (InvalidTupleException invalidTuple){
                 System.out.println(invalidTuple.getMessage()+"\n");
             }catch (InvalidStringExecption e){
                 System.out.println("DFA brokes for the string :"+e.getMessage());
+            }catch (NoSuchTrassitionException e){
+                System.out.println("Invalid DFA :"+e.getMessage());
             }
         }
     }
 
-    private void check(Boolean expectation,Alphabets alphabets, DFA dfa) throws InvalidStringExecption {
+    private void check(Boolean expectation,Alphabets alphabets, DFA dfa) throws InvalidStringExecption, NoSuchTrassitionException {
         if(expectation != dfa.check(alphabets))
             throw new InvalidStringExecption(alphabets);
+    }
+
+    private void runSingleString(DfaGenerator dfaGenerator, JSONObject jsonObject) throws InvalidTupleException, NoSuchTrassitionException, InvalidStringExecption {
+        DFA dfa = dfaGenerator.fromJson((JSONObject) jsonObject.get("tuple"));
+        ArrayList<Alphabets> passCases = dfaGenerator.parseCases((JSONArray) jsonObject.get("pass-cases"));
+        for (Alphabets alphabets : passCases) {
+            check(true, alphabets, dfa);
+        }
+
+        ArrayList<Alphabets> failCases = dfaGenerator.parseCases((JSONArray) jsonObject.get("fail-cases"));
+        for (Alphabets alphabets : failCases) {
+            check(false, alphabets, dfa);
+        }
     }
 }
